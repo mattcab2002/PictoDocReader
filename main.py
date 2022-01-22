f79e54bb93a125ec55d419870091c87a0d64ee77
Matthew Cabral <matthew.larbac@gmail.com>
2022-01-22 14:24:16 -0500

    Merge conflict fix

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from matplotlib.image import imread
 import numpy as np
-<<<<<<< HEAD
 import sys
-=======
->>>>>>> image-conversion
 
 imgArray = imread("images/img1.png")
 imgSize = imgArray.shape
@@ -14,8 +11,6 @@ docArray = imread("documents/doc1.png")
 docSize = docArray.shape
 docHeight = len(docArray)
 docWidth = len(docArray[0])
-<<<<<<< HEAD
-=======
 
 
 def pixelsMatch(docRow, docCol, imgRow, imgCol):
@@ -30,40 +25,10 @@ def pixelsMatch(docRow, docCol, imgRow, imgCol):
 
 def validateCorners(row, col):
     if (pixelsMatch(row, col, 0, 0)):  # top left
-        if (pixelsMatch(row, col+imgWidth-1, 0, -1)):  # bottom left
-            if (pixelsMatch(row+imgHeight-1, col, -1, 0)):  # top right
-                if (pixelsMatch(row+imgHeight-1, col+imgWidth-1, -1, -1)):  # bottom right
-                    return True
-    else:
-        return False
-
-
->>>>>>> pdf-conversion
-
-
-def pixelsMatch(docRow, docCol, imgRow, imgCol):
-    if (docRow >= docHeight) or (docCol >= docWidth):
-        return False
-    minlen = min(len(docArray[docRow][docCol]), len(imgArray[imgRow][imgCol]))
-    for i in range(minlen):
-        if (docArray[docRow][docCol][i] != imgArray[imgRow][imgCol][i]):
-            return False
-    return True
-
-
-<<<<<<< HEAD
-def validateCorners(row, col):
-    if (pixelsMatch(row, col, 0, 0)):  # top left
-<<<<<<< HEAD
         if (pixelsMatch(row, col+imgWidth-1, 0, -1)):  # bottom left
             if (pixelsMatch(row+imgHeight-1, col, -1, 0)):  # top right
                 if (pixelsMatch(row+imgHeight-1, col+imgWidth-1, -1, -1)):  # bottom right
                     print("Found Image")
-=======
-        if (pixelsMatch(row, col + imgWidth - 1, 0, -1)):  # bottom left
-            if (pixelsMatch(row + imgHeight - 1, col, -1, 0)):  # top right
-                if (pixelsMatch(row + imgHeight - 1, col + imgWidth - 1, -1, -1)):  # bottom right
->>>>>>> image-conversion
                     return True
     else:
         return False
@@ -74,17 +39,12 @@ def validateFullImage(row, col, docArray, docHeight, docWidth):
         for j in range(imgWidth):
             try:
                 # check all elements of doc to image
-<<<<<<< HEAD
                 if (pixelsMatch(row + i, col + j, i, j)):
-=======
-                if (pixelsMatch(row + i, col + j, i, j, docArray, docHeight, docWidth)):
->>>>>>> image-conversion
                     continue
                 else:
                     return False
             except IndexError:
                 return False
-<<<<<<< HEAD
     print("Found Image")
     return True
 
@@ -94,45 +54,17 @@ def diagonalSearch(row, col, docArray, docHeight, docWidth):
     for i in range(imgHeight):
         try:
             if not (pixelsMatch(row + i, col + j, i, j)):
-=======
-    return True
-
-
-def diagonalSearch1(row, col, docArray, docHeight, docWidth):
-    j = 0
-    for i in range(imgHeight):
-        try:
-            if ((not pixelsMatch(row + i, col + j, i, j, docArray, docHeight, docWidth))):
->>>>>>> image-conversion
                 return False
             else:
                 j += 1
                 continue
         except IndexError:
             return False
-<<<<<<< HEAD
     print("Found Image")
-=======
-    return True
-
-
-def diagonalSearch2(doc, row, col, img, imgWidth, imgHeight, docArray, docHeight, docWidth):
-    j = imgWidth - 1
-    for i in range(imgHeight - 1, 0, -1):
-        try:
-            if (doc[row + i][col + j] != img[i][j]).all():
-                return False
-            else:
-                j -= 1
-                continue
-        except IndexError:
-            return False
->>>>>>> image-conversion
     return True
 
 
 def main():
-<<<<<<< HEAD
     for row in range(docHeight-imgHeight+1):
         for col in range(docWidth-imgWidth+1):
             if (pixelsMatch(row, col, 0, 0)):  # find first instance of correct pixel
@@ -145,35 +77,7 @@ def main():
                 else:
                     print("Invalid command")
                     exit(0)
-=======
-    pass
->>>>>>> image-conversion
 
 
 if __name__ == "__main__":
     main()
-=======
-                if ((currentDoc != currentImg).all()):
-                    print("break 1")
-                    break
-                if ((i == imgHeight - 1) and (j == imgWidth - 1)):
-                    print("found image")
-
-                    break
-                if (j + 1 >= imgWidth):
-                    if ((i + 1) < imgHeight):
-                        i += 1
-                        if ((i2+1) >= docHeight):
-                            print("break 2")
-                            break
-                        i2 = i2 + 1
-                        j = 0
-                        j2 = col
-                else:
-                    j = j+1
-                    if ((j2 + 1) >= docWidth):
-                        print("break 3")
-                        break
-                    j2 += 1
-            print(row, col)
->>>>>>> pdf-conversion
